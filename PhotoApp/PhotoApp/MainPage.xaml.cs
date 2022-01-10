b3ef8f6c0093b793f59aecb3cd28fa05bd0715c4
unknown <steve.lorent@gmail.com>
2022-01-10 11:44:30 +0100

    demo

---
+++ PhotoApp/PhotoApp/MainPage.xaml.cs
@@ -0,0 +1,55 @@
+﻿using Plugin.Media;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace PhotoApp
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        private async void TakeAPhotoButton_OnClicked(object sender, EventArgs e)
+        {
+            if (!CrossMedia.Current.IsCameraAvailable || !CrossMedia.Current.IsTakePhotoSupported)
+            {
+                await DisplayAlert("No Camera", ":( No camera avaialble.", "OK");
+                return;
+            }
+
+            var file = await CrossMedia.Current.TakePhotoAsync(new Plugin.Media.Abstractions.StoreCameraMediaOptions
+            {
+                PhotoSize = Plugin.Media.Abstractions.PhotoSize.Small,
+                Directory = "Sample",
+                Name = "test.jpg", 
+                SaveToAlbum = true
+            });
+
+            if (file == null)
+            {
+                return;
+            }
+               
+            
+            //await DisplayAlert("File Location", file.Path, "OK");
+
+            image.Source = ImageSource.FromStream(() =>
+            {
+                var stream = file.GetStream();
+                file.Dispose();
+                return stream;
+            });
+    }
+
+        public MainPage()
+        {
+            InitializeComponent();
+            
+        }
+    }
+}
